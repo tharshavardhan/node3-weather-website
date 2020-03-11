9e7e4a1cdca1285f5a0932f0f7e940a109130c0f
harshavardhant <harshavardhan.t@mobisy.com>
2020-03-11 14:26:43 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.get("/about", (req, res) => {
 app.get("/help", (req, res) => {
     res.render('help', {
         helpText: "this is some helpful text ",
-        title: 'Helpage'
+        title: 'Help Page'
     })
 })
 
